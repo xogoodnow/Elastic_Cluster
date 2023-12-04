c59b080fe0f529e9f678703b3bb4ee198191dab1
xogoodnow <xo4ever9999@gmail.com>
2023-12-04 14:20:15 +0330

    The output file for this terraform module

---
+++ Terraform/modules/servers/outputs.tf
@@ -0,0 +1,17 @@
+output "elastic_ips" {
+  value = hcloud_server.elastic[*].ipv4_address
+}
+
+output "elastic_private_ips" {
+  value = hcloud_server.osd[*].network[*].ip
+}
+
+output "monitoring_ips" {
+  value = hcloud_server.monitoring[*].network[*].ip
+}
+
+
+output "monitoring_private_ips" {
+  value = hcloud_server.mon[*].network[*].ip
+}
+
