cf4e8e8b687e3ea22310838a877615186b9fd1b4
xogoodnow <xo4ever9999@gmail.com>
2023-12-04 14:18:23 +0330

    Added the main file to create servers :rocket:

---
+++ Terraform/modules/servers/main.tf
@@ -0,0 +1,70 @@
+resource "hcloud_server" "elastic" {
+  count = 3
+  name         = "elastic-${count.index}"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id]
+  location = var.location
+  network {
+    network_id = data.hcloud_network.private-network.id
+  }
+
+}
+
+
+
+resource "hcloud_server" "monitoring" {
+  count = 1
+  name         = "monitoring-${count.index}"
+  image        = var.image_name
+  server_type  = "cx21"
+  ssh_keys = [data.hcloud_ssh_key.key1.id]
+  location = var.location
+  network {
+    network_id = data.hcloud_network.private-network.id
+  }
+}
+
+
+data "hcloud_network" "private-network" {
+  name = "elastic-internal"
+
+}
+
+
+data "hcloud_ssh_key" "key1"  {
+  name = "kang"
+
+}
+
+
+
+resource "local_file" "inventory" {
+  content = templatefile("${path.module}/inventory.tpl",
+    {
+      elastic_ips = hcloud_server.elastic.*.ipv4_address
+      monitoring_ips = hcloud_server.monitoring.*.ipv4_address
+    }
+  )
+  filename = "${path.module}/../../inventory.yaml"
+}
+
+
+resource "local_file" "etcd-hosts" {
+  content  = templatefile("${path.module}/etchost.tpl",
+    {
+      elastic_0_private_ips = hcloud_server.mon.0.network[*].ip
+      elastic_1_private_ips = hcloud_server.mon.1.network[*].ip
+      elastic_2_private_ips = hcloud_server.mon.2.network[*].ip
+
+      monitoring_private_ips = hcloud_server.monitoring.0.network[*].ip
+
+    }
+  )
+  filename = "${path.module}/../../../Ansible/roles/Ceph_Pre_Setup/files/etchost.yaml"
+}
+
+
+
+
+
