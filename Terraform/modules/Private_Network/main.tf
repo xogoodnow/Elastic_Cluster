608a5e25a12d93da41182af4cc63ba6b93cb1ee6
xogoodnow <xo4ever9999@gmail.com>
2023-12-04 18:09:47 +0330

    Fixed an issue :bug:

--- Terraform/modules/Private_Network/main.tf
+++ Terraform/modules/Private_Network/main.tf
@@ -1,11 +1,11 @@
-resource "hcloud_network" "ceph-internal" {
+resource "hcloud_network" "elastic-internal" {
   name     = "elastic-internal"
   ip_range = "10.0.0.0/8"
 
 }
 
-resource "hcloud_network_subnet" "ceph-internal-network" {
-  network_id   = hcloud_network.ceph-internal.id
+resource "hcloud_network_subnet" "elastic-internal-network" {
+  network_id   = hcloud_network.elastic-internal.id
   type         = "cloud"
   network_zone = "eu-central"
   ip_range     = "10.0.0.0/8"
